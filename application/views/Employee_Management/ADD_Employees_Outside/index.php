e55e1d6ad69868010ab2e679e0066398a42285d9
Pasindu Ramesh <pasinduramesh277@gmail.com>
2025-07-21 07:47:58 +0530

    Employees_Outside_Part_v016

--- application/views/Employee_Management/ADD_Employees_Outside/index.php
+++ application/views/Employee_Management/ADD_Employees_Outside/index.php
@@ -1583,8 +1583,7 @@
             });
         </script> -->
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+       
 
         <!-- <script>
             let selectedQualifications = [];
@@ -1924,7 +1923,8 @@
                 clearFormFields();
             }
         </script> -->
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         <script>
